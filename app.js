fcf9cc0b67e61d4f62f3e71df0ffb28571a16e05
anuragbansal <anuragbits01@gmail.com>
2024-02-24 06:15:38 +0530

    added mousefollwer effect

---
+++ app.js
@@ -0,0 +1,33 @@
+let body = document.querySelector("body");
+let mouseFollower = document.querySelector("#mouse-follower");
+let allAnchor = document.querySelectorAll("a");
+let allBtns = document.querySelectorAll("button");
+
+body.addEventListener("mousemove", function(e){
+    mouseFollower.style.left = `${e.clientX}px`
+    mouseFollower.style.top = `${e.clientY}px`
+})
+
+allAnchor.forEach(function(a){
+    a.addEventListener("mouseover", function(){
+        mouseFollower.innerText = "Click";
+    })
+})
+
+allAnchor.forEach(function(a){
+    a.addEventListener("mouseleave", function(){
+        mouseFollower.innerText = ""
+    })
+})
+
+allBtns.forEach(function(btn){
+    btn.addEventListener("mouseover", function(){
+        mouseFollower.innerText = "Click"
+    })
+})
+
+allBtns.forEach(function(btn){
+    btn.addEventListener("mouseleave", function(){
+        mouseFollower.innerText = ""
+    })
+})
